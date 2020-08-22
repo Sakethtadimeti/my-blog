ef86704022e9d91885e8416c38904d444e79d13a
Saketh Tadimeti <saketh.tadimeti@sabre.com>
2020-08-22 13:22:58 +0530

    inital commit

---
+++ src/build/precache-manifest.02cef0e9ea922f7f78f803b31f50cb58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c568080a848f6978ddd",
+    "url": "/static/js/main.85ce6359.chunk.js"
+  },
+  {
+    "revision": "f68f7937a277a9d1be07",
+    "url": "/static/js/2.ed707dbc.chunk.js"
+  },
+  {
+    "revision": "8c568080a848f6978ddd",
+    "url": "/static/css/main.83553cdd.chunk.css"
+  },
+  {
+    "revision": "a828b7f757209dda6a43377fcbc027f3",
+    "url": "/index.html"
+  }
+];
